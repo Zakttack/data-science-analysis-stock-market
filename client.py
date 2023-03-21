f71ee6bf76e7f5b37dda42d5ba1050fc1efdf947
Zakttack <zak.merrigan@ndsu.edu>
2023-03-21 13:01:21 -0500

    updated dataset

--- client.py
+++ client.py
@@ -1,6 +1,17 @@
 import csv;
+import matplotlib.pyplot as plt;
+dates = []
+close = []
 with open('stock_market_dataset.csv') as file:
     csvFile = csv.reader(file)
+    count = 0
     for lines in csvFile:
-        print(lines)
-
+        if count > 0:
+            dates.append(lines[0])
+            close.append(lines[4])
+        count = count + 1
+fig, ax = plt.subplots()
+ax.bar(dates, close)
+ax.set_xlabel('Date')
+ax.set_ylabel('Price')
+plt.show()
